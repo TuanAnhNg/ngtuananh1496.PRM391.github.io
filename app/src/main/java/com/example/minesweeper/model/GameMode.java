52b18f1aa6a9f2b2c5c3a47a0a3c974c398da7cb
TuanAnhNg <anhnt.it@tripi.vn>
2020-07-16 10:52:59 +0700

    Revert "add project"
    This reverts commit d7fa4bb602dce7b8393ea3011b12762b660de1d0.

--- app/src/main/java/com/example/minesweeper/model/GameMode.java
+++
@@ -1,7 +0,0 @@
-package com.example.minesweeper.model;
-
-public class GameMode {
-    public static final String EASY = "Easy";
-    public static final String MEDIUM = "Medium";
-    public static final String EXPERT = "Expert";
-}
